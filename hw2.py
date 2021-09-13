f227d3a52e246346485552373b1bf66c8033f7d1
jasonpereira96 <jasonpereira074@gmail.com>
2021-09-12 21:20:34 -0500

    adding changes

--- hw2.py
+++ hw2.py
@@ -20,7 +20,9 @@ def initialize(N):
 
   return w0, w1, w2, X
 
-def plot_initial_graph():
+def plot_initial_graph(w0, w1, w2, X):
+  plt.clf()
+
   S0 = [] # (e)
   S1 = [] # (f)
 
@@ -46,7 +48,7 @@ def plot_initial_graph():
   plt.plot(x, y, label="Boundary")
   plt.xlabel('x1')
   plt.ylabel('x2')
-  plt.suptitle('Graphical intepretation of a perceptron')
+  plt.suptitle('Graphical intepretation of a perceptron (N = ' + str(N) + ')')
   plt.legend()
   plt.show() # (g)
 
@@ -63,7 +65,6 @@ def get_num_of_misclassifications(correct_weights, actual_weights, points):
 
   misclassifications = 0
   correct_classifications = 0
-  count = 1
 
   for point in points:
     x1, x2 = point
@@ -80,25 +81,10 @@ def get_num_of_misclassifications(correct_weights, actual_weights, points):
     else:
       misclassifications = misclassifications + 1
 
-    p1 = 'positive' if c_desired >= 0 else 'negative'
-    p2 = 'positive' if c_real >= 0 else 'negative'
-
-    if both_positive or both_negative:
-      p3 = ''
-    else:
-      p3 = 'misclassification'
-
-    t = Template('$p1 | $p2 | $p3')
-
-    # print(str(count) + ' ' + t.substitute(p1=p1, p2=p2, p3=p3))
-    count = count + 1
-
   return misclassifications
 
 def train():
   global w0_, w1_, w2_, w0, w1, w2
-  # w0, w1, w2 = correct_weights
-  # w0_, w1_, w2_ = actual_weights
   epoch = 0
   misclassifications_per_epoch = []
 
@@ -106,7 +92,6 @@ def train():
     misclassifications = 0
     for point in X:
       x1, x2 = point
-
       desired_output = step(w0 + w1*x1 + w2*x2)
       actual_output = step(w0_ + w1_*x1 + w2_*x2)
 
@@ -131,57 +116,137 @@ def show_misclassifications_graph(misclassifications_per_epoch):
   x = list(range(number_of_epochs))
   y = misclassifications_per_epoch
   plt.plot(x, y, label="Misclassifications per epoch")
-  plt.suptitle('Misclassifications per epoch')
+  plt.suptitle('Misclassifications per epoch (\u03B7 = ' + str(eta) + ' and N = ' + str(N) + ')')
   plt.xlabel('Epoch')
   plt.ylabel('Misclassifications')
   plt.legend()
   plt.show()
 
 def print_weights_comparison():
-  print('initial weights: ' + str(initial_weights))
-  print('final weights: ' + str([w0_, w1_, w2_]))
-  print('Difference between initial and final w0: ' + str(abs(initial_weights[0] - w0_)))
-  print('Difference between initial and final w1: ' + str(abs(initial_weights[1] - w1_)))
-  print('Difference between initial and final w2: ' + str(abs(initial_weights[2] - w2_)))
+  print('Initial weights: ' + str([w0, w1, w2]))
+  print('Final weights: ' + str([w0_, w1_, w2_]))
+  print('Difference between optimal and final w0: ' + str(abs(abs(w0) - abs(w0_))))
+  print('Difference between optimal and final w1: ' + str(abs(abs(w1) - abs(w1_))))
+  print('Difference between optimal and final w2: ' + str(abs(abs(w2) - abs(w2_))))
 
 
-N = 100
+rows = []
+N = 0
+eta = 0
 
-w0, w1, w2, X = initialize(N)
-plot_initial_graph()
+def run():
+  row = []
+  global N, w0, w1, w2, w0_, w1_, w2_, X, eta
+  N = 100
+  w0, w1, w2, X = initialize(N)
+  plot_initial_graph(w0, w1, w2, X)
 
-eta = 1 # i
-misclassifications_per_epoch = train()
-show_misclassifications_graph(misclassifications_per_epoch)
+  eta = 1 # i
+  misclassifications_per_epoch = train()
+  show_misclassifications_graph(misclassifications_per_epoch)
+  row.append(len(misclassifications_per_epoch))
 
-print_weights_comparison()
-
-w0_, w1_, w2_ = initial_weights
-eta = 10
-misclassifications_per_epoch = train()
-show_misclassifications_graph(misclassifications_per_epoch)
-
-
-w0_, w1_, w2_ = initial_weights
-eta = 0.1
-misclassifications_per_epoch = train()
-show_misclassifications_graph(misclassifications_per_epoch)
-
-N = 1000
-w0, w1, w2, X = initialize(N)
-plot_initial_graph()
-
-eta = 1 # i
-misclassifications_per_epoch = train()
-show_misclassifications_graph(misclassifications_per_epoch)
-
-w0_, w1_, w2_ = initial_weights
-eta = 10
-misclassifications_per_epoch = train()
-show_misclassifications_graph(misclassifications_per_epoch)
+  print_weights_comparison()
+  plot_initial_graph(w0_, w1_, w2_, X)
 
 
-w0_, w1_, w2_ = initial_weights
-eta = 0.1
-misclassifications_per_epoch = train()
-show_misclassifications_graph(misclassifications_per_epoch)
+  w0_, w1_, w2_ = initial_weights
+  eta = 10
+  misclassifications_per_epoch = train()
+  show_misclassifications_graph(misclassifications_per_epoch)
+  row.append(len(misclassifications_per_epoch))
+
+
+  w0_, w1_, w2_ = initial_weights
+  eta = 0.1
+  misclassifications_per_epoch = train()
+  show_misclassifications_graph(misclassifications_per_epoch)
+  row.append(len(misclassifications_per_epoch))
+
+
+  N = 1000
+  w0, w1, w2, X = initialize(N)
+  plot_initial_graph(w0, w1, w2, X)
+
+
+  eta = 1 # i
+  misclassifications_per_epoch = train()
+  show_misclassifications_graph(misclassifications_per_epoch)
+  row.append(len(misclassifications_per_epoch))
+
+
+  w0_, w1_, w2_ = initial_weights
+  eta = 10
+  misclassifications_per_epoch = train()
+  show_misclassifications_graph(misclassifications_per_epoch)
+  row.append(len(misclassifications_per_epoch))
+
+
+
+  w0_, w1_, w2_ = initial_weights
+  eta = 0.1
+  misclassifications_per_epoch = train()
+  show_misclassifications_graph(misclassifications_per_epoch)
+  row.append(len(misclassifications_per_epoch))
+
+  rows.append(row)
+
+
+for i in range(1):
+  run()
+  if i % 10 == 0:
+    print(i)
+
+print(rows)
+
+'''
+(h) vii
+initial weights: [-0.03384998158526087, -0.29792879896350755, -0.8273876976548766]
+final weights: [-1.033849981585261, -3.1701347875342694, 1.2139549075551104]
+Difference between initial and final w0: 1.0000000000000002
+Difference between initial and final w1: 2.872205988570762
+Difference between initial and final w2: 2.0413426052099872
+
+The initial weights w0', w1' and w2' can be different from the optimal weights. 
+However, the lines w0 + w1x + w2y = 0 and w0' + w1'x + w2'y = 0 represent almost the same
+line when viewed on a graph
+
+
+After running the perceptron training algorithm 2000 times, the following results were observed:
+
+Parameters: N = 100 and eta = 1
+Average epochs needed until convergence: 80
+Median epochs needed until convergence: 12
+
+Parameters: N = 100 and eta = 10
+Average epochs needed until convergence: 60
+Median epochs needed until convergence: 13
+
+Parameters: N = 100 and eta = 0.1
+Average epochs needed until convergence: 55
+Median epochs needed until convergence: 11
+
+Parameters: N = 1000 and eta = 1
+Average epochs needed until convergence: 251
+Median epochs needed until convergence: 34
+
+Parameters: N = 1000 and eta = 10
+Average epochs needed until convergence: 282
+Median epochs needed until convergence: 34
+
+Parameters: N = 1000 and eta = 0.1
+Average epochs needed until convergence: 241
+Median epochs needed until convergence: 33
+
+(l)
+Changing the value of eta does not have too much effect on the number of epochs needed until convergence.
+
+(m)
+Sometimes our initial choice of weights is indeed unlucky and thus the algorithm takes a rather long time to converge.
+However, this happens only rarely. If we start with a different set of initial weights, then training time does vary slightly.
+However, as long as we do not change eta or N, it is not a large difference.
+
+(n)
+As N increases, the number of epochs needed until convergence increases propotionally.
+'''
+
