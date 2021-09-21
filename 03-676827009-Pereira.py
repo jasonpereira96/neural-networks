258768082d8f7a62a9861073d62dcffb19222135
jasonpereira96 <jasonpereira074@gmail.com>
2021-09-21 18:59:48 -0500

    fixing argmax bug

--- 03-676827009-Pereira.py
+++ 03-676827009-Pereira.py
@@ -4,10 +4,15 @@ import matplotlib.pyplot as plt
 
 image_size = 28
 DIMENSIONS = image_size * image_size
-N = 5
+N = 50
+eta =  1 # learning rate
+EPSILON = 0.01
 
 def step(x):
-  return 1 if x >= 0 else 0
+  u = lambda x: 1 if x >= 0 else 0
+  if isinstance(x, int):
+    return u(x)
+  return np.vectorize(u)(x)
 
 def argmax(V):
   return np.argmax(V)
@@ -19,6 +24,44 @@ def get_desired_output_matrix(digit):
   matrix = np.zeros((10, 1))
   matrix[digit] = 1
   return matrix
+
+def print_delta(delta):
+  over_zero = []
+  for x in np.nditer(delta):
+    if x > 0:
+      over_zero.append(x)
+  print(over_zero)
+
+def get_weights_diff(W1, W2):
+  diffs = []
+  for row_index in range(10):
+    for col_index in range(DIMENSIONS):
+      delta = W1[row_index][col_index] - W2[row_index][col_index]
+      if delta != 0:
+        diffs.append(delta)
+  return diffs
+
+def print_image(image):
+  # image = training_images[2]
+  for row in range(28):
+    line = ''
+    for col in range(28):
+      pixel = image[row][col]
+      if pixel != 0:
+        line = line + '@'
+      else:
+        line = line + '.'
+    print(line)
+
+def get_initial_weights():
+  # return np.random.rand(10, DIMENSIONS)
+  weights = np.zeros((10, DIMENSIONS), dtype=float)
+
+  for row_index in range(10):
+    for col_index in range(DIMENSIONS):
+      weights[row_index][col_index] = np.random.uniform(-1, 1)
+
+  return weights
 
 def read_label_data():
   f = gzip.open('train-labels-idx1-ubyte.gz','r')
@@ -37,7 +80,6 @@ def read_label_data():
   for i in range(N):
     label = int.from_bytes(f.read(1), signed=False, byteorder='big')
     test_labels.append(label)
-
   return training_labels, test_labels
   
 def read_image_data():
@@ -76,63 +118,62 @@ class HW3:
     self.test_images = test_images
     self.training_labels = training_labels
     self.test_labels = test_labels
-    self.W = np.random.rand(10, DIMENSIONS)
+    # self.W = np.random.rand(10, DIMENSIONS)
+    self.W = get_initial_weights()
     self.errors_per_epoch = []
     self.epoch = 0
   
   def update_weights(self):
-    W = self.W
-
-    for index in range(1):
-      X = vectorize(self.training_images[index])
-      desired_output_digit = self.training_labels[index].reshape()
+    for index in range(N):
+      old_weights = np.copy(self.W)
+      image = self.training_images[index]
+      X = vectorize(image)
+      desired_output_digit = self.training_labels[index]
       desired_output = get_desired_output_matrix(desired_output_digit)
+      # print_image(image)
       
-      W = W + eta * (desired_output - step(matmul(W, X)))
+      WX = np.matmul(self.W, X)
+      delta = eta * np.matmul((desired_output - step(WX)), np.transpose(X))
+      # print('delta')
+      # print_delta(delta)
+      self.W = self.W + eta * np.matmul((desired_output - step(WX)), np.transpose(X))
+
+      print(get_weights_diff(old_weights, self.W))
 
   def run_epoch(self):
     errors = 0
-    for index in range(1):
+    for index in range(N):
       X = vectorize(self.training_images[index])
-      W = self.W
-      V = np.matmul(W, X)
-      print(V)
+      V = np.matmul(self.W, X)
+      # print(V)
       actual_output = argmax(V)
-      desired_output = argmax(self.training_labels[index])
-      delta = desired_output - actual_output
+      desired_output = self.training_labels[index]
 
-      if delta != 0:
+      if desired_output != actual_output:
         errors = errors + 1
 
-    return errors
-  
-    # image = training_images[2]
-
-    # for row in range(28):
-    #   line = ''
-    #   for col in range(28):
-    #     pixel = image[row][col]
-    #     if pixel != 0:
-    #       line = line + '@'
-    #     else:
-    #       line = line + '.'
-    #   print(line)
+    return errors  
 
   def train(self):
-    count = 1
+    print('initial W')
+    print(self.W)
+    print('------------------')
 
-    while count > 0: # True
+    while True: # True
       errors = self.run_epoch()
       self.errors_per_epoch.append(errors)
       self.epoch = self.epoch + 1
 
-      if errors == 0:
+      if (errors / N) < EPSILON:
         print('training complete')
         break
       else:
         self.update_weights()
 
-      count = 0
+      if self.epoch % 10 == 0:
+        print(self.epoch)
+        # print(self.W)
+        print('errors: ' + str(errors))
 
 training_images, test_images, training_labels, test_labels = read_data()
 hw3 = HW3(training_images, test_images, training_labels, test_labels)
