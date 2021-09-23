771429bc201a1eefd2f6b2276d1fdd26b4bfbd72
jasonpereira96 <jasonpereira074@gmail.com>
2021-09-22 22:29:29 -0500

    adding comments and other changes to hw3

--- 03-676827009-Pereira.py
+++ 03-676827009-Pereira.py
@@ -1,12 +1,21 @@
 import gzip
 import numpy as np
 import matplotlib.pyplot as plt
+from os.path import exists
 
-image_size = 28
-DIMENSIONS = image_size * image_size
-N = 50
-eta =  1 # learning rate
-EPSILON = 0.01
+# Note:
+# The gzip files must be present in the same directory for this script to run
+
+FILENAME = 'weights.npy'
+IMAGE_SIZE = 28
+DIMENSIONS = IMAGE_SIZE * IMAGE_SIZE
+
+# Change the values of these parameters according to what is required
+N = 1000
+test_N = 10000
+convergence_margin = 0
+eta = 0.7 # learning rate
+EPSILON = 0
 
 def step(x):
   u = lambda x: 1 if x >= 0 else 0
@@ -25,13 +34,6 @@ def get_desired_output_matrix(digit):
   matrix[digit] = 1
   return matrix
 
-def print_delta(delta):
-  over_zero = []
-  for x in np.nditer(delta):
-    if x > 0:
-      over_zero.append(x)
-  print(over_zero)
-
 def get_weights_diff(W1, W2):
   diffs = []
   for row_index in range(10):
@@ -41,8 +43,8 @@ def get_weights_diff(W1, W2):
         diffs.append(delta)
   return diffs
 
+# function to print an image on to the console
 def print_image(image):
-  # image = training_images[2]
   for row in range(28):
     line = ''
     for col in range(28):
@@ -53,59 +55,60 @@ def print_image(image):
         line = line + '.'
     print(line)
 
-def get_initial_weights():
-  # return np.random.rand(10, DIMENSIONS)
-  weights = np.zeros((10, DIMENSIONS), dtype=float)
+# function to save the weights to a file for future use
+def save_weights(W):
+  print('saving weights to file')
+  np.save(FILENAME, W)
 
+# function to initialize W. If from_file is True, the we try to load the weights from a file
+def get_initial_weights(from_file=False):
+  if from_file and exists(FILENAME):
+    print('loading initial weights from file')
+    return np.load(FILENAME)
+
+  weights = np.zeros((10, DIMENSIONS), dtype=float)
   for row_index in range(10):
     for col_index in range(DIMENSIONS):
       weights[row_index][col_index] = np.random.uniform(-1, 1)
 
   return weights
 
+# functions to read data from the gzip files. The gzip files must be present in the same directory
 def read_label_data():
   f = gzip.open('train-labels-idx1-ubyte.gz','r')
-  # reading the # magic number,  number of labels
+  # reading the # magic number, number of labels
   a = f.read(8)
   training_labels = []
   for i in range(N):
     label = int.from_bytes(f.read(1), signed=False, byteorder='big')
     training_labels.append(label)
-  # print(training_labels)
 
   f = gzip.open('t10k-labels-idx1-ubyte.gz','r')
-  # reading the # magic number,  number of labels
+  # reading the # magic number, number of labels
   a = f.read(8)
   test_labels = []
-  for i in range(N):
+  for i in range(test_N):
     label = int.from_bytes(f.read(1), signed=False, byteorder='big')
     test_labels.append(label)
+  
   return training_labels, test_labels
   
 def read_image_data():
   f = gzip.open('train-images-idx3-ubyte.gz','r')
-  # reading the # magic number,  number of images,  rows , columns
+  # reading the # magic number, number of images, rows, columns
   a = f.read(16)
-  buf = f.read(image_size * image_size * N)
+  buf = f.read(IMAGE_SIZE * IMAGE_SIZE * N)
   data = np.frombuffer(buf, dtype=np.uint8).astype(np.float32)
-  training_images = data.reshape(N, image_size, image_size)
-  image = np.asarray(training_images[4]).squeeze()
-  plt.imshow(image)
-  # plt.show()
+  training_images = data.reshape(N, IMAGE_SIZE, IMAGE_SIZE)
+
   f = gzip.open('t10k-images-idx3-ubyte.gz','r')
-  # reading the # magic number,  number of images,  rows , columns
+  # reading the # magic number, number of images, rows, columns
   a = f.read(16)
-  buf = f.read(image_size * image_size * N)
+  buf = f.read(IMAGE_SIZE * IMAGE_SIZE * test_N)
   data = np.frombuffer(buf, dtype=np.uint8).astype(np.float32)
-  test_images = data.reshape(N, image_size, image_size)
-  image = np.asarray(test_images[3]).squeeze()
-  # plt.imshow(image)
+  test_images = data.reshape(test_N, IMAGE_SIZE, IMAGE_SIZE)
   return training_images, test_images
   
-
-# add a case if gzip files are not present
-# change to prevent plagarism
-
 def read_data():
   training_images, test_images = read_image_data()
   training_labels, test_labels = read_label_data()
@@ -118,27 +121,20 @@ class HW3:
     self.test_images = test_images
     self.training_labels = training_labels
     self.test_labels = test_labels
-    # self.W = np.random.rand(10, DIMENSIONS)
-    self.W = get_initial_weights()
+    self.W = get_initial_weights(from_file=False)
     self.errors_per_epoch = []
     self.epoch = 0
   
   def update_weights(self):
     for index in range(N):
-      old_weights = np.copy(self.W)
       image = self.training_images[index]
       X = vectorize(image)
       desired_output_digit = self.training_labels[index]
       desired_output = get_desired_output_matrix(desired_output_digit)
-      # print_image(image)
       
       WX = np.matmul(self.W, X)
-      delta = eta * np.matmul((desired_output - step(WX)), np.transpose(X))
-      # print('delta')
-      # print_delta(delta)
-      self.W = self.W + eta * np.matmul((desired_output - step(WX)), np.transpose(X))
 
-      print(get_weights_diff(old_weights, self.W))
+      self.W = self.W + eta * np.matmul((desired_output - step(WX)), np.transpose(X))
 
   def run_epoch(self):
     errors = 0
@@ -154,27 +150,63 @@ class HW3:
 
     return errors  
 
+  def test(self):
+    errors = 0
+    for index in range(test_N):
+      X = vectorize(self.test_images[index])
+      V = np.matmul(self.W, X)
+      # print(V)
+      actual_output = argmax(V)
+      desired_output = self.test_labels[index]
+
+      if desired_output != actual_output:
+        errors = errors + 1
+
+    error_perc = (errors / test_N) * 100
+    print('Percentage of misclassified samples = ' + str(error_perc) + '%')
+    return errors  
+
+
   def train(self):
     print('initial W')
     print(self.W)
     print('------------------')
 
-    while True: # True
+    while True:
       errors = self.run_epoch()
       self.errors_per_epoch.append(errors)
       self.epoch = self.epoch + 1
 
-      if (errors / N) < EPSILON:
-        print('training complete')
+      if (errors / self.epoch) <= eta:
+        print('Training complete')
+        print('Errors per epoch:')
+        print(self.errors_per_epoch)
+        # save_weights(self.W)
         break
       else:
         self.update_weights()
 
       if self.epoch % 10 == 0:
-        print(self.epoch)
-        # print(self.W)
+        print('Epoch: ' + str(self.epoch))
         print('errors: ' + str(errors))
+        # saving the weights to file periodically
+        # save_weights(self.W)
 
-training_images, test_images, training_labels, test_labels = read_data()
-hw3 = HW3(training_images, test_images, training_labels, test_labels)
-hw3.train()
+  def plot_errors_vs_epoch(self):
+    plt.clf()
+    number_of_epochs = len(self.errors_per_epoch)
+    x = list(range(number_of_epochs))
+    y = self.errors_per_epoch
+    plt.plot(x, y, label="Misclassifications per epoch")
+    plt.suptitle('Misclassifications per epoch (\u03B7 = ' + str(eta) + ' and N = ' + str(N) + ' and \u0190 = ' + str(EPSILON) + ')')
+    plt.xlabel('Epoch')
+    plt.ylabel('Misclassifications')
+    plt.legend()
+    plt.show()
+    
+if __name__ == "__main__":
+  training_images, test_images, training_labels, test_labels = read_data()
+  hw3 = HW3(training_images, test_images, training_labels, test_labels)
+  hw3.train()
+  hw3.plot_errors_vs_epoch()
+  hw3.test()
