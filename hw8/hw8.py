5b624ca83c613220c02197196c69ed6ecf68a317
jasonpereira96 <jasonpereira074@gmail.com>
2021-11-21 12:59:32 -0600

    board

--- hw8/hw8.py
+++ hw8/hw8.py
@@ -1,4 +1,14 @@
-import math
+import numpy as np
+
+UP = "UP"
+DOWN = "DOWN"
+LEFT = "LEFT"
+RIGHT = "RIGHT"
+N = 5
+Y = 0.9
+LR = 0.1
+
+actions = [UP, DOWN, LEFT, RIGHT]
 
 class Board:
   def __init__(self, N):
@@ -13,8 +23,7 @@ class Board:
       res = res + str(self.board[i]) + '\n'
     return res
      
-    
-  def set_value(self, x, y, v):
+  def get_phys(self, x, y):
     x = x - 1
     y = y - 1
 
@@ -22,17 +31,37 @@ class Board:
     phys_y = y
     phys_x = N - x -1
 
-    print("x: {} y: {} px: {} py: {}".format(x, y, phys_x, phys_y))
+    return phys_x, phys_y
+
+  def set_value(self, x, y, v):
+    phys_x, phys_y = self.get_phys(x, y)
     self.board[phys_x][phys_y] = v
+  
+  def get_value(self, x, y):
+    phys_x, phys_y = self.get_phys(x, y)
+    return self.board[phys_x][phys_y]
 
-N = 4
-board = Board(N)
-counter = 1
-for i in range(N):
-  for j in range(N):
-    board.set_value(i+1, j+1, i+1+j+1)
-    counter += 1
 
+def init_q_table():
+  board = Board(N)
+  n_entries = N * N * len(actions)
+  entries = np.random.normal(0, 1, n_entries)
+  counter = 0
+  for i in range(N):
+    for j in range(N):
+      values = {}
+      for action in actions:
+        values[action] = entries[counter]
+        counter += 1
+      board.set_value(i+1, j+1, values)
+
+  return board
+      
+
+board = init_q_table()
 print(board)
 
 
+
+# fuck
+# instead of N*N states, may require N*N*2 or N*N*3 states
